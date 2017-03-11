442bdeb1994a57e3562cae3137ba12f191af7b5a
Cauê Alves <ceasbz2@gmail.com>
2017-03-11 17:32:19 -0300

    feat: export to use a module

--- index.js
+++ index.js
@@ -8,11 +8,27 @@ const tokens = {
 	access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
 
-const getTwitterFollowers = require('get-twitter-followers');
+module.exports = (username, followersLimit) => {
+	const usernameTypeOf = typeof username;
+	if (usernameTypeOf !== 'string') {
+        throw new TypeError(`Expected a string but got ${usernameTypeOf}`);
+    }
 
-getTwitterFollowers(tokens, 'ceasbz').then(followers => {
-	followers.sort((a, b) => b.followers_count - a.followers_count);
-	followers.slice(0, 10).map(follower => {
-		console.log(`https://twitter.com/${follower.screen_name} - Total: ${follower.followers_count}`);
+    const followersLimitTypeOf = typeof followersLimit;
+    if (followersLimitTypeOf !== 'number') {
+    	throw new TypeError(`Expected a string but got ${followersLimitTypeOf}`);	
+    }
+
+	const getTwitterFollowers = require('get-twitter-followers');
+
+	getTwitterFollowers(tokens, username).then(followers => {
+		followers.sort((a, b) => b.followers_count - a.followers_count);
+		followers.slice(0, followersLimit).map((follower, index) => {
+			console.log(`
+ Position: ${index + 1}
+ Link: https://twitter.com/${follower.screen_name}
+ Total followers: ${follower.followers_count}`
+);
+		});
 	});
-});
+};
